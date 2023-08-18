6f0ebb6e57b6f24838ea479f1177c73bd20317db
Avishka Nilupul <121239381+AvishkaNilupul@users.noreply.github.com>
2023-08-18 19:18:44 +0530

    Version 2
    1. Log in system added with login coockies(Hashmap login)
    2. Song import system added
    3. UI updated (working on progress)

--- Media Player/src/main/java/com/example/demo/Controller.java
+++ Media Player/src/main/java/com/example/demo/Controller.java
@@ -1,10 +1,17 @@
 package com.example.demo;
 
+import java.awt.event.*;
+import java.io.*;
+import javax.swing.*;
+
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -12,6 +19,8 @@ import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.io.File;
@@ -19,10 +28,27 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.ResourceBundle;
+import javafx.scene.Node;
+
 
 
 
 public class Controller implements Initializable {
+    private Scene scene;
+
+    public void login (ActionEvent event) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LogIn.fxml"));
+
+        //Parent root = FXMLLoader.load()(getClass().getResource("LogIn.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.centerOnScreen();
+        scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
 
     @FXML
     private ListView <String> myListView;
@@ -61,7 +87,7 @@ public class Controller implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
 
-        directory = new File("C:\\Users\\Avishka\\Documents\\Java Applications\\demo\\src\\main\\music");
+        directory = new File("C:\\Users\\Avishka\\Documents\\Java Applications\\Media Player\\src\\main\\music");
 
 
         songs = new ArrayList<File>();
@@ -101,6 +127,7 @@ public class Controller implements Initializable {
                     mediaPlayer = new MediaPlayer(media);
 
                     songLabel.setText(songs.get(songNumber).getName());
+                    mediaPlayer.play();
 
                     songNumber = 1;
 
@@ -135,6 +162,119 @@ public class Controller implements Initializable {
 
     }
 
+    public void impo() throws IOException {
+
+        File filepath = null;
+
+        String MName = null;
+
+        JFileChooser fileChooser = new JFileChooser();
+
+        fileChooser.setCurrentDirectory(new File(".")); //sets current directory
+
+        int response = fileChooser.showOpenDialog(null); //select file to open
+        //int response = fileChooser.showSaveDialog(null); //select file to save
+
+        if (response == JFileChooser.APPROVE_OPTION) {
+            filepath = new File(fileChooser.getSelectedFile().getAbsolutePath());
+            MName = filepath.getName();
+
+
+        }
+        File destFile = new File("C:\\Users\\Avishka\\Documents\\Java Applications\\Media Player\\src\\main\\music\\" + MName);
+
+
+
+       FileInputStream fileInputStream = null;
+        FileOutputStream fileOutputStream = null;
+
+
+        fileInputStream = new FileInputStream(filepath);
+        fileOutputStream = new FileOutputStream(destFile);
+
+        int i = 0;
+
+        while ((i = fileInputStream.read()) !=-1){
+            fileOutputStream.write(i);
+
+        }
+
+        Resume();
+
+
+
+
+
+    }
+
+       /* if (filepath.exists()){
+            try (FileInputStream is = new FileInputStream(filepath); FileOutputStream os = new FileOutputStream(destFile)){
+                if (!filepath.canRead()){
+                    filepath.setReadable(true);
+                    filepath.setWritable(true);
+                }
+                if (!destFile.canWrite()){
+                    destFile.setReadable(true);
+                    destFile.setWritable(true);
+                }
+                int len;
+                float srcFileSize = is.available()/1000.0f;
+                float totalCopied = 0.0f;
+                byte[] bytes = new byte[1024];
+                while((len = is.read(bytes))>0){
+                    os.write(bytes, 0,len);
+                    totalCopied += len;
+                    System.out.print("\rCopied..."+ totalCopied/1000.0f + "kb/" +srcFileSize+"kb");
+                    Thread.sleep(5);
+
+                }
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        */
+
+    void Resume(){
+
+        myListView.getItems().clear();
+
+        directory = new File("C:\\Users\\Avishka\\Documents\\Java Applications\\Media Player\\src\\main\\music");
+
+
+        songs = new ArrayList<File>();
+
+        files = directory.listFiles();
+
+
+        if (files != null) {
+
+            for (File file : files) {
+                songs.add(file);
+                myListView.getItems().add(String.valueOf(file));
+                media = new Media(songs.get(songNumber).toURI().toString());
+
+            }
+        }
+
+
+
+    }
+
+
+
+    public void test() throws IOException {
+
+        FileReader reader = new FileReader("C:\\Users\\Avishka\\Documents\\Java Applications\\Media Player\\src\\main\\loginfo\\h.txt");
+        int data = reader.read();
+        while(data != -1) {
+            System.out.print((char)data);
+            data = reader.read();
+        }
+        reader.close();
+
+    }
+
 
     public void nextMedia() {
 
@@ -151,6 +291,8 @@ public class Controller implements Initializable {
 
                 songLabel.setText(songs.get(songNumber).getName());
 
+                mediaPlayer.play();
+
                 if (songNumber == songs.size()){
 
                     songNumber = 0;
@@ -164,7 +306,7 @@ public class Controller implements Initializable {
 
 
 
-                directory = new File("C:\\Users\\Avishka\\Documents\\Java Applications\\demo\\src\\main\\music");
+                directory = new File("C:\\Users\\Avishka\\Documents\\Java Applications\\Media Player\\src\\main\\music");
 
 
                 songs = new ArrayList<File>();
@@ -184,6 +326,8 @@ public class Controller implements Initializable {
                 mediaPlayer = new MediaPlayer(media);
                 songLabel.setText(songs.get(songNumber).getName());
 
+                mediaPlayer.play();
+
 
                 } else {
                 songNumber = 0;
@@ -195,6 +339,7 @@ public class Controller implements Initializable {
                 mediaPlayer = new MediaPlayer(media);
 
                 songLabel.setText(songs.get(songNumber).getName());
+                mediaPlayer.play();
 
 
             }
--- Media Player/src/main/java/com/example/demo/Main.java
+++ Media Player/src/main/java/com/example/demo/Main.java
@@ -4,22 +4,79 @@ package com.example.demo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Main extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Spotify Clone");
-        stage.setScene(scene);
-        stage.show();
+
+        if (new LogT().a){
+
+                FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("playArea.fxml"));
+                Scene scene = new Scene(fxmlLoader.load());
+                stage.setTitle("Rexpotify");
+                stage.centerOnScreen();
+                scene.getStylesheets().add(getClass().getResource("app.css").toExternalForm());
+                stage.setScene(scene);
+                //String css = this.getClass().getResource("app.css").toExternalForm();
+                //scene.getStylesheets().add(css);
+                stage.show();
+
+
+
+
+
+
+
+        }else {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LogIn.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setTitle("Rexpotify");
+            stage.centerOnScreen();
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 
-    public static void main(String[] args) {
-        launch();
+    public static void main(String[] args) throws IOException {
+
+            launch();
+
+
+
     }
+}
+
+
+
+class LogT {
+
+
+    static char sdata ;
+    boolean a = false;
+    ID loga = new ID();
+
+    HashMap<String, String> logininfo = loga.getLogininfo();
+
+
+     LogT() throws IOException {
+
+         BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Avishka\\Documents\\Java Applications\\Media Player\\src\\main\\loginfo\\h.txt"));
+
+        //FileReader reader = new FileReader();
+
+         String ab = (reader.readLine());
+
+         if (logininfo.containsKey(ab)){
+             a= true;
+         }
+
+     }
 }
